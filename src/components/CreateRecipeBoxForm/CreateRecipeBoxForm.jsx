6cf8906c286d8cc6340b7cc16eadcc374418f5d0
azhhng <azhhng@gmail.com>
2023-05-28 14:57:11 -0400

    add functionality to create own recipe box

---
+++ src/components/CreateRecipeBoxForm/CreateRecipeBoxForm.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
+
+function CreateRecipeBoxForm() {
+  const { user } = useAuth0();
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const createRecipeBox = async () => {
+    const response = await axios.post(
+      `${process.env.REACT_APP_API_ADDRESS}/${user.sub}/recipe-box`,
+      {
+        name,
+        description,
+      }
+    );
+    console.log("Creating a recipe box...");
+    console.log(response);
+  };
+
+  return (
+    <div className="recipe-box-form-container">
+      <div>
+        <form className="recipe-box-form">
+          <input
+            type="text"
+            id="recipe-box-name"
+            name="name/"
+            value={name}
+            placeholder="Name..."
+            onChange={(event) => setName(event.target.value)}
+          ></input>
+          <input
+            type="text"
+            id="recipe-box-description"
+            name="description/"
+            value={description}
+            placeholder="Description..."
+            onChange={(event) => setDescription(event.target.value)}
+          ></input>
+        </form>
+      </div>
+      <button onClick={() => createRecipeBox()}>
+        <span>Create Recipe Box</span>
+      </button>
+    </div>
+  );
+}
+
+export default CreateRecipeBoxForm;
--- src/components/RecipeBox/RecipeBox.jsx
+++ src/components/RecipeBox/RecipeBox.jsx
@@ -2,7 +2,7 @@ import "./RecipeBox.scss";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Link } from "react-router-dom";
+import CreateRecipeForm from "../CreateRecipeForm/CreateRecipeForm";
 
 function RecipeBox(props) {
   const { user } = useAuth0();
@@ -24,9 +24,7 @@ function RecipeBox(props) {
     <div className="recipe-box">
       <h3 className="recipe-box-name">{props.box.name}</h3>
       <h3>{props.box.description}</h3>
-      {/* <h3>
-          <Link to={link}>About</Link> \\ <a href={props.box.github}>Github</a>
-        </h3> */}
+      <CreateRecipeForm recipeBox={props.box} />
     </div>
   );
 }
