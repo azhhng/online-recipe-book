6646876ede57d3ea9d2308dc6b2f2abc8d0273a0
azhhng <azhhng@gmail.com>
2023-05-29 18:32:40 -0400

    prepare recipe page template and route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
-import WelcomePage from "./pages/WelcomePage";
+import WelcomePage from "./pages/WelcomePage/WelcomePage";
 import Header from "./components/Header/Header";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import RecipePage from "./pages/RecipePage/RecipePage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<WelcomePage />} />
           <Route path="/profile/:user" element={<ProfilePage />} />
+          <Route path="/profile/:user/recipes" element={<RecipePage />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/RecipePage/RecipePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./RecipePage.scss";
+import ProfileHeader from "../../components/ProfileHeader/ProfileHeader";
+
+function RecipePage() {
+  return (
+    <div className="recipe-page-container">
+      <ProfileHeader />
+      RecipePage
+    </div>
+  );
+}
+
+export default RecipePage;
