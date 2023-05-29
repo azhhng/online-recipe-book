f6848fbfd157755d9a907412f96a2f9985149cc1
azhhng <azhhng@gmail.com>
2023-05-29 18:31:47 -0400

    change emoji files to svg instead of png

---
+++ src/enums/Emojis.jsx
@@ -0,0 +1,15 @@
+// Enum for trip request payment methods
+export const Emojis = Object.freeze({
+  BANANA: "banana",
+  BEER: "beer_mug",
+  BENTO: "bento_box",
+  BIRTHDAY_CAKE: "birthday_cake",
+  BREAD: "bread",
+  BUBBLE_TEA: "bubble_tea",
+  CHEESE_WEDGE: "cheese_wedge",
+  CHERRIES: "cherries",
+  CROISSANT: "croissant",
+  CORN: "ear_of_corn",
+  DANGO: "dango",
+  HOT_DOG: "hot_dog",
+});
