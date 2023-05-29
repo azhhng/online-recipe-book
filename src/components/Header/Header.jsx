ed3d955de3426cd7b371ce8c02650e9f33764d6e
azhhng <azhhng@gmail.com>
2023-05-28 21:38:36 -0400

    add webpage title to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,17 @@ import "./Header.scss";
 import LoginButton from "../LoginButton/LoginButton";
 import LogoutButton from "../LogoutButton/LogoutButton";
 import { useAuth0 } from "@auth0/auth0-react";
+import bentoBoxIcon from "../../assets/images/food/bento_box_3d.png";
 
 function Header() {
   const { user } = useAuth0();
 
   return (
     <div className="header">
+      <img id="bento-icon" src={bentoBoxIcon} alt="ponyo" />
+      <Link to={"/"} className="title-link">
+        <span>Recipe</span>boxd
+      </Link>
       <Link to={"/"}>Home</Link>
       {!user && <LoginButton />}
       {user && <Link to={"/profile/" + user.sub.split("|")[1]}>Profile</Link>}
