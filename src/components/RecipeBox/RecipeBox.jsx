83ab193c579782b296fe8b7126c4efea68c7d453
azhhng <azhhng@gmail.com>
2023-05-28 21:39:29 -0400

    check number of recipes per box

--- src/components/RecipeBox/RecipeBox.jsx
+++ src/components/RecipeBox/RecipeBox.jsx
@@ -1,12 +1,17 @@
 import "./RecipeBox.scss";
-import React from "react";
+import React, { useEffect } from "react";
 import CreateRecipeForm from "../CreateRecipeForm/CreateRecipeForm";
 
 function RecipeBox(props) {
+  useEffect(() => {
+    console.log(props);
+  }, [props]);
+
   return (
     <div className="recipe-box">
       <h3 className="recipe-box-name">{props.box.name}</h3>
       <h3>{props.box.description}</h3>
+      <h3># Recipes: {props.recipes.length}</h3>
       <CreateRecipeForm recipeBox={props.box} />
     </div>
   );
