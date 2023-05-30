f24dec10cd33e67a4e11efa4a2fc8ee974fd0599
azhhng <azhhng@gmail.com>
2023-05-29 23:24:47 -0400

    redesign landing page

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./WelcomePage.scss";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import Emoji from "../../components/Emoji/Emoji";
+import { Emojis } from "../../enums/Emojis";
 
 function WelcomePage() {
   return (
@@ -9,7 +11,21 @@ function WelcomePage() {
         <Sidebar />
       </div>
       <div className="guide-container">
-        <h3>Get started with 3 simple steps:</h3>
+        <h1>Get started in 3 simple steps:</h1>
+        <ul>
+          <li>
+            <Emoji name={Emojis.WATERMELON} width={35} height={35} />
+            Sign up with your Google or Github account
+          </li>
+          <li>
+            <Emoji name={Emojis.WATERMELON} width={35} height={35} />
+            Create your first recipe box
+          </li>
+          <li>
+            <Emoji name={Emojis.WATERMELON} width={35} height={35} />
+            Start adding your recipe links
+          </li>
+        </ul>
       </div>
     </div>
   );
