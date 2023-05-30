a40f5e592d79a33a20aec8cd1a104368d8580508
azhhng <azhhng@gmail.com>
2023-05-30 01:36:32 -0400

    add data to recipe cards

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,13 +1,20 @@
 import "./Recipe.scss";
 import React from "react";
+import Emoji from "../Emoji/Emoji";
+import { FoodEmoji, SymbolEmoji } from "../../enums/Emojis";
 
 function Recipe(props) {
-  console.log("in recipe component...");
-  console.log(props);
   return (
     <div className="recipe-container">
       <h3 className="recipe-name-container">{props.name}</h3>
+      <h3>{props.link}</h3>
       <h3>{props.description}</h3>
+      {props.favorite && (
+        <Emoji name={SymbolEmoji.SPARKLING_HEART} width={50} height={35} />
+      )}
+      {props.hasMade && (
+        <Emoji name={FoodEmoji.FORK_KNIFE_PLATE} width={50} height={35} />
+      )}
     </div>
   );
 }
