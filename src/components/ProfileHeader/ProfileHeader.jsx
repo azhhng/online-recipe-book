76619d1df90666b3f7b9be94161482dd2f1aeb29
azhhng <azhhng@gmail.com>
2023-05-30 01:35:54 -0400

    use url to get user sub over calling auth0

--- src/components/ProfileHeader/ProfileHeader.jsx
+++ src/components/ProfileHeader/ProfileHeader.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./ProfileHeader.scss";
-import { useAuth0 } from "@auth0/auth0-react";
 
 function ProfileHeader() {
-  const { user } = useAuth0();
-  // TODO fix bug where refreshing page gives undefined sub error
+  const location = useLocation();
+  const userSub = location.pathname.split("/")[2];
 
   return (
     <div className="profile-header">
       <Link to={"/"}>Recipe Boxes</Link>
-      <Link to={"/profile/" + user.sub.split("|")[1] + "/recipes"}>
-        Recipes
-      </Link>
+      <Link to={"/profile/" + userSub + "/recipes"}>Recipes</Link>
     </div>
   );
 }
