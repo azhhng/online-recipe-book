3ce4c9e59b6a3a85dbc07882b8606a69d187b1f1
azhhng <azhhng@gmail.com>
2023-06-01 08:50:57 -0400

    add endpoint to update recipe box

--- src/components/RecipeBox/RecipeBox.jsx
+++ src/components/RecipeBox/RecipeBox.jsx
@@ -1,14 +1,31 @@
 import "./RecipeBox.scss";
 import React from "react";
+import axios from "axios";
 import CreateRecipeForm from "../CreateRecipeForm/CreateRecipeForm";
 
 function RecipeBox(props) {
+  const updateRecipeBox = async () => {
+    // TODO allow users to choose how they update their recipe box
+    const response = await axios.put(
+      `${process.env.REACT_APP_API_ADDRESS}/recipe-box/${props.box.recipe_box_id}`,
+      {
+        description: "Some of my favorite desserts!",
+        name: "Yummy Desserts",
+      }
+    );
+    console.log("Updating a recipe...");
+    console.log(response);
+  };
+
   return (
     <div className="recipe-box">
       <h3 className="recipe-box-name">{props.box.name}</h3>
       <h3>{props.box.description}</h3>
       <h3># Recipes: {props.recipes.length}</h3>
       <CreateRecipeForm recipeBox={props.box} sourcePage={"RecipeBox"} />
+      <button onClick={() => updateRecipeBox()}>
+        <span>Update Recipe Box</span>
+      </button>
     </div>
   );
 }
