353c61727aaf652514803786d89b27fb3c444dd6
azhhng <azhhng@gmail.com>
2023-06-01 08:45:05 -0400

    remove unused code

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,7 +7,7 @@ function Sidebar() {
   return (
     <div className="sidebar-container">
       <div className="title-container">
-        <Emoji name={FoodEmoji.BENTO} width={140} height={100} />
+        <Emoji name={FoodEmoji.BENTO} width={100} height={100} />
         <h1>
           <span>Recipe</span>boxd
         </h1>
@@ -15,13 +15,6 @@ function Sidebar() {
 
       <h2>Your collection of online recipes...</h2>
       <h2>The modern recipe box!</h2>
-      {/* <h2>
-        Made by <a href="https://azhhng.netlify.app">Alice Zhang</a>.
-      </h2>
-      <h2>
-        You can find the Github{" "}
-        <a href="https://github.com/azhhng/spaceify">here</a>.
-      </h2> */}
     </div>
   );
 }
