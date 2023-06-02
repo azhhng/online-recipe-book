822887d2313ad388df51ee55096a11b32fbb7b2b
azhhng <azhhng@gmail.com>
2023-06-02 00:08:12 -0400

    add add, update, and delete options for recipe boxes

--- src/components/RecipeBox/RecipeBox.jsx
+++ src/components/RecipeBox/RecipeBox.jsx
@@ -1,9 +1,14 @@
 import "./RecipeBox.scss";
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import CreateRecipeForm from "../CreateRecipeForm/CreateRecipeForm";
+import Emoji from "../Emoji/Emoji";
+import { SymbolEmoji } from "../../enums/Emojis";
 
 function RecipeBox(props) {
+  const [addingRecipeToBox, setAddingRecipeToBox] = useState(false);
+  const [editingRecipeBox, setEditingRecipeBox] = useState(false);
+
   const updateRecipeBox = async () => {
     // TODO allow users to choose how they update their recipe box
     const response = await axios.put(
@@ -17,12 +22,37 @@ function RecipeBox(props) {
     console.log(response);
   };
 
+  const addRecipeToBox = async () => {
+    setAddingRecipeToBox(true);
+  };
+
+  const deleteRecipeBox = async () => {
+    // TODO create box that alerts users that all recipes are going to be deleted as well
+    const response = await axios.delete(
+      `${process.env.REACT_APP_API_ADDRESS}/recipe-box/${props.box.recipe_box_id}`
+    );
+    console.log(response);
+    console.log("Deleting a recipe...");
+  };
+
+  // TODO fix tool tip
   return (
     <div className="recipe-box">
+      <button onClick={() => addRecipeToBox()}>
+        <Emoji name={SymbolEmoji.PAGE} width={30} height={30} />
+      </button>
+      <button onClick={() => updateRecipeBox()}>
+        <Emoji name={SymbolEmoji.PENCIL} width={30} height={30} />
+      </button>
+      <button onClick={() => deleteRecipeBox()}>
+        <Emoji name={SymbolEmoji.FIRE} width={30} height={30} />
+      </button>
       <h3 className="recipe-box-name">{props.box.name}</h3>
       <h3>{props.box.description}</h3>
       <h3># Recipes: {props.recipes.length}</h3>
-      <CreateRecipeForm recipeBox={props.box} sourcePage={"RecipeBox"} />
+      {addingRecipeToBox && (
+        <CreateRecipeForm recipeBox={props.box} sourcePage={"RecipeBox"} />
+      )}
       <button onClick={() => updateRecipeBox()}>
         <span>Update Recipe Box</span>
       </button>
