22a68a4fff158e70d69b15de57236004952b59ab
azhhng <azhhng@gmail.com>
2023-06-05 12:22:49 -0400

    move sub header to main header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Header.scss";
 import LoginButton from "../LoginButton/LoginButton";
 import LogoutButton from "../LogoutButton/LogoutButton";
@@ -7,11 +7,25 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Emoji from "../Emoji/Emoji";
 import { FoodEmoji } from "../../enums/Emojis";
 
-function Header() {
+function Header(props) {
+  const location = useLocation();
+  const isOnProfile =
+    location.pathname.split("/")[1] === "profile" ? true : false;
   const { user } = useAuth0();
+  console.log(isOnProfile);
 
   return (
     <div className="header">
+      {isOnProfile && (
+        <Link to={"/profile/" + location.pathname.split("/")[2]}>
+          Recipe Boxes
+        </Link>
+      )}
+      {isOnProfile && (
+        <Link to={"/profile/" + location.pathname.split("/")[2] + "/recipes"}>
+          Recipes
+        </Link>
+      )}
       <Emoji type={"food"} name={FoodEmoji.BENTO} width={50} height={35} />
       <Link to={"/"}>Home</Link>
       {!user && <LoginButton />}
