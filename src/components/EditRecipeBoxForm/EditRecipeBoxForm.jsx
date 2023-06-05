cc35c6ee5585692987f77a9e342568fae9d51c73
azhhng <azhhng@gmail.com>
2023-06-05 12:27:31 -0400

    allow user to edit color of recipe box

--- src/components/EditRecipeBoxForm/EditRecipeBoxForm.jsx
+++ src/components/EditRecipeBoxForm/EditRecipeBoxForm.jsx
@@ -5,17 +5,19 @@ import "./EditRecipeBoxForm.scss";
 function EditRecipeBoxForm(props) {
   const [name, setName] = useState(props.box.name);
   const [description, setDescription] = useState(props.box.description);
+  const [color, setColor] = useState(props.box.color);
   const fieldsToUpdate = {};
 
   const updateRecipeBox = async () => {
-    // TODO allow users to choose how they update their recipe box
     const response = await axios.put(
       `${process.env.REACT_APP_API_ADDRESS}/recipe-box/${props.box.recipe_box_id}`,
       {
         description: description,
         name: name,
+        color: color,
       }
     );
+    props.setEditingRecipeBox(false);
     console.log("Updating a recipe...");
     console.log(response);
   };
@@ -27,7 +29,6 @@ function EditRecipeBoxForm(props) {
           <input
             type="text"
             id="recipe-box-name"
-            name="name/"
             value={name}
             placeholder="Name..."
             onChange={(event) => {
@@ -35,16 +36,21 @@ function EditRecipeBoxForm(props) {
               setName(event.target.value);
             }}
           ></input>
-          <input
+          <textarea
             type="text"
             id="recipe-box-description"
-            name="description/"
             value={description}
             placeholder="Description..."
             onChange={(event) => {
               fieldsToUpdate["description"] = event.target.value;
               setDescription(event.target.value);
             }}
+          ></textarea>
+          <input
+            type="color"
+            id="favcolor"
+            value={color}
+            onChange={(event) => setColor(event.target.value)}
           ></input>
         </form>
       </div>
