88d0318a46abd6f4e1c01ac1bdf887bf5b959d25
azhhng <azhhng@gmail.com>
2023-06-05 12:24:08 -0400

    add custom color to recipe box emoji

--- src/components/RecipeBox/RecipeBox.jsx
+++ src/components/RecipeBox/RecipeBox.jsx
@@ -9,6 +9,24 @@ import { SymbolEmoji } from "../../enums/Emojis";
 function RecipeBox(props) {
   const [addingRecipeToBox, setAddingRecipeToBox] = useState(false);
   const [editingRecipeBox, setEditingRecipeBox] = useState(false);
+  const darkerColor = adjustBrightness(props.box.color, -80);
+
+  // from @supersan https://stackoverflow.com/a/57401891
+  function adjustBrightness(color, amount) {
+    return (
+      "#" +
+      color
+        .replace(/^#/, "")
+        .replace(/../g, (color) =>
+          (
+            "0" +
+            Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(
+              16
+            )
+          ).slice(-2)
+        )
+    );
+  }
 
   const deleteRecipeBox = async () => {
     // TODO create box that alerts users that all recipes are going to be deleted as well
@@ -22,30 +40,42 @@ function RecipeBox(props) {
   // TODO fix tool tip
   return (
     <div className="recipe-box">
-      <button onClick={() => setAddingRecipeToBox(true)}>
-        <Emoji
-          type={"symbols"}
-          name={SymbolEmoji.PAGE}
-          width={30}
-          height={30}
-        />
-      </button>
-      <button onClick={() => setEditingRecipeBox(true)}>
-        <Emoji
-          type={"symbols"}
-          name={SymbolEmoji.PENCIL}
-          width={30}
-          height={30}
-        />
-      </button>
-      <button onClick={() => deleteRecipeBox()}>
-        <Emoji
-          type={"symbols"}
-          name={SymbolEmoji.FIRE}
-          width={30}
-          height={30}
-        />
-      </button>
+      <Emoji
+        type={"food"}
+        name={props.box.emoji}
+        width={30}
+        height={30}
+        style={{
+          backgroundColor: props.box.color,
+          border: `2px solid ${darkerColor}`,
+        }}
+      />
+      <div className="actions-box">
+        <button onClick={() => setAddingRecipeToBox(true)}>
+          <Emoji
+            type={"symbols"}
+            name={SymbolEmoji.PLUS}
+            width={30}
+            height={30}
+          />
+        </button>
+        <button onClick={() => setEditingRecipeBox(true)}>
+          <Emoji
+            type={"symbols"}
+            name={SymbolEmoji.PENCIL}
+            width={30}
+            height={30}
+          />
+        </button>
+        <button onClick={() => deleteRecipeBox()}>
+          <Emoji
+            type={"symbols"}
+            name={SymbolEmoji.FIRE}
+            width={30}
+            height={30}
+          />
+        </button>
+      </div>
       <h3 className="recipe-box-name">{props.box.name}</h3>
       <h3>{props.box.description}</h3>
       <h3># Recipes: {props.recipes.length}</h3>
