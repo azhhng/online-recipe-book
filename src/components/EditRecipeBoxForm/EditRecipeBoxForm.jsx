cef1600eafb9407dfdcf4fc376a478cea381b862
azhhng <azhhng@gmail.com>
2023-06-10 01:15:41 -0400

    allow user to edit recipe box's emoji

--- src/components/EditRecipeBoxForm/EditRecipeBoxForm.jsx
+++ src/components/EditRecipeBoxForm/EditRecipeBoxForm.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./EditRecipeBoxForm.scss";
+import Emoji from "../Emoji/Emoji";
+import EmojiPicker from "../EmojiPicker/EmojiPicker";
 
 function EditRecipeBoxForm(props) {
   const [name, setName] = useState(props.box.name);
   const [description, setDescription] = useState(props.box.description);
   const [color, setColor] = useState(props.box.color);
+  const [emoji, setEmoji] = useState(props.box.emoji);
+  const [openEmojiPicker, setOpenEmojiPicker] = useState(false);
+  let emojiColor = "#fad017";
+
   const fieldsToUpdate = {};
 
   const updateRecipeBox = async () => {
@@ -13,6 +19,7 @@ function EditRecipeBoxForm(props) {
       `${process.env.REACT_APP_API_ADDRESS}/recipe-box/${props.box.recipe_box_id}`,
       {
         description: description,
+        emoji: emoji,
         name: name,
         color: color,
       }
@@ -46,6 +53,33 @@ function EditRecipeBoxForm(props) {
               setDescription(event.target.value);
             }}
           ></textarea>
+          <div
+            className="emoji-holder"
+            onClick={(e) => {
+              e.preventDefault();
+              setOpenEmojiPicker(true);
+            }}
+          >
+            <Emoji
+              type={"food"}
+              name={emoji}
+              width={30}
+              height={30}
+              style={{
+                border: `2px solid ${emojiColor}`,
+                padding: "5px",
+                borderRadius: "10px",
+                cursor: "pointer",
+                margin: "5px",
+              }}
+            />
+          </div>
+          {openEmojiPicker && (
+            <EmojiPicker
+              setEmoji={setEmoji}
+              setOpenEmojiPicker={setOpenEmojiPicker}
+            />
+          )}
           <input
             type="color"
             id="favcolor"
