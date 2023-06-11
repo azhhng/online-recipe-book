8f37e0cf859082a580b1bf8085256035e9c4f939
azhhng <azhhng@gmail.com>
2023-06-10 22:44:17 -0400

    allow user to edit existing recipe

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,18 +1,36 @@
 import "./Recipe.scss";
 import React, { useEffect, useState } from "react";
 import Emoji from "../Emoji/Emoji";
+import axios from "axios";
 import { FoodEmoji, SymbolEmoji } from "../../enums/Emojis";
 import { adjustBrightness } from "../../helpers/colorHelpers";
+import EditRecipeForm from "../EditRecipeForm/EditRecipeForm";
+import ActionsBar from "../ActionsBar/ActionsBar";
 
 function Recipe(props) {
   const [darkerColor, setDarkerColor] = useState("#fff");
+  const [editingRecipe, setEditingRecipe] = useState(false);
 
   useEffect(() => {
     setDarkerColor(adjustBrightness(props.box.color, -80));
   }, [props]);
 
+  const deleteRecipe = async () => {
+    const response = await axios.delete(
+      `${process.env.REACT_APP_API_ADDRESS}/recipe/${props.recipeId}`
+    );
+    console.log(response);
+    console.log("Deleting a recipe...");
+    window.location.reload();
+  };
+
   return (
     <div className="recipe-container">
+      <ActionsBar
+        source={"Recipe"}
+        setEditing={setEditingRecipe}
+        delete={deleteRecipe}
+      />
       <h3 className="recipe-name-container">{props.name}</h3>
       <h4>{props.description}</h4>
       <div className="recipe-box-tag">
@@ -55,6 +73,18 @@ function Recipe(props) {
           height={30}
         />
       </a>
+      {editingRecipe && (
+        <EditRecipeForm
+          recipeId={props.recipeId}
+          name={props.name}
+          favorite={props.favorite}
+          hasMade={props.hasMade}
+          description={props.description}
+          link={props.link}
+          box={props.box}
+          setEditingRecipe={setEditingRecipe}
+        />
+      )}
     </div>
   );
 }
