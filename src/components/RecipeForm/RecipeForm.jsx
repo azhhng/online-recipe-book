fcd482155a6dbb29e8d0649cc99922fc69a1b6c0
azhhng <azhhng@gmail.com>
2023-06-12 14:08:33 -0400

    reduce create and edit recipe forms into one component

--- src/components/RecipeForm/RecipeForm.jsx
+++ src/components/RecipeForm/RecipeForm.jsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
-import "./CreateRecipeForm.scss";
+import "./RecipeForm.scss";
 
-function CreateRecipeForm(props) {
+function RecipeForm(props) {
   const { user } = useAuth0();
+  const [name, setName] = useState(props?.name ?? "");
+  const [description, setDescription] = useState(props?.description ?? "");
+  const [link, setLink] = useState(props?.link ?? "");
+  const [hasMade, setHasMade] = useState(props?.hasMade ?? false);
+  const [favorite, setFavorite] = useState(props?.favorite ?? false);
   const [recipeBoxId, setRecipeBoxId] = useState(
-    props?.recipeBox?.recipe_box_id ?? ""
+    props?.box?.recipe_box_id ?? props?.recipeBox?.recipe_box_id ?? ""
   );
-  const [name, setName] = useState("");
-  const [link, setLink] = useState("");
-  const [description, setDescription] = useState("");
-  const [hasMade, setHasMade] = useState(false);
-  const [favorite, setFavorite] = useState(false);
   const [boxOptions, setBoxOptions] = useState([]);
 
   const createRecipe = async () => {
@@ -32,18 +32,35 @@ function CreateRecipeForm(props) {
     window.location.reload();
   };
 
+  const updateRecipe = async () => {
+    const response = await axios.put(
+      `${process.env.REACT_APP_API_ADDRESS}/recipe/${props.recipeId}`,
+      {
+        description,
+        name,
+        link,
+        recipe_box_id: recipeBoxId,
+        favorite,
+        has_made: hasMade,
+      }
+    );
+    props.setEditingRecipe(false);
+    console.log("Updating a recipe...");
+    console.log(response);
+    window.location.reload();
+  };
+
   useEffect(() => {
+    // TODO have the actual recipe box set not just the first one
     const getRecipeBoxes = async () => {
       const recipeBoxes = (
         await axios.get(
           `${process.env.REACT_APP_API_ADDRESS}/${user?.sub}/recipe-box`
         )
       ).data;
-
-      if (recipeBoxes.length !== 0) {
+      if (props.action === "create" && recipeBoxes.length !== 0) {
         setRecipeBoxId(recipeBoxes[0].recipe_box_id);
       }
-
       const options = recipeBoxes.map((recipeBox) => {
         return (
           <option key={recipeBox.recipe_box_id} value={recipeBox.recipe_box_id}>
@@ -54,10 +71,15 @@ function CreateRecipeForm(props) {
       setBoxOptions(options);
     };
     getRecipeBoxes();
-  }, [user]);
+  }, [user, props.action]);
 
   return (
-    <div className="recipe-form-container">
+    <div
+      className="recipe-form-container"
+      style={
+        props.action === "edit" ? { position: "absolute", zIndex: "5" } : {}
+      }
+    >
       <div>
         <form className="recipe-form">
           <input
@@ -65,30 +87,34 @@ function CreateRecipeForm(props) {
             id="recipe-name"
             value={name}
             placeholder="Name..."
-            onChange={(event) => setName(event.target.value)}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
           ></input>
           <input
             type="text"
             id="recipe-link"
             value={link}
             placeholder="Link..."
-            onChange={(event) => setLink(event.target.value)}
+            onChange={(event) => {
+              setLink(event.target.value);
+            }}
           ></input>
           <textarea
             type="text"
             id="recipe-description"
             value={description}
             placeholder="Description..."
-            onChange={(event) => setDescription(event.target.value)}
+            onChange={(event) => {
+              setDescription(event.target.value);
+            }}
           ></textarea>
-          {props?.sourcePage === "RecipePage" && (
-            <select
-              id="recipe-box-id"
-              onChange={(event) => setRecipeBoxId(event.target.value)}
-            >
-              {boxOptions}
-            </select>
-          )}
+          <select
+            id="recipe-id"
+            onChange={(event) => setRecipeBoxId(event.target.value)}
+          >
+            {boxOptions}
+          </select>
           <input
             type="checkbox"
             id="recipe-has-made"
@@ -102,20 +128,25 @@ function CreateRecipeForm(props) {
             checked={favorite}
             onChange={(event) => setFavorite(!favorite)}
           ></input>
-          <label htmlFor="recipe-favorite">Favorite</label>
         </form>
       </div>
-      {props?.sourcePage !== "RecipePage" && (
-        <button onClick={() => props.setAddingRecipeToBox(false)}>
+      {props.action === "edit" && (
+        <button onClick={() => props.setEditingRecipe(false)}>
           <span>Cancel</span>
         </button>
       )}
-
-      <button onClick={() => createRecipe()}>
-        <span>Add</span>
-      </button>
+      {props.action === "edit" && (
+        <button onClick={() => updateRecipe()}>
+          <span>Update</span>
+        </button>
+      )}
+      {props.action === "create" && (
+        <button onClick={() => createRecipe()}>
+          <span>Add</span>
+        </button>
+      )}
     </div>
   );
 }
 
-export default CreateRecipeForm;
+export default RecipeForm;
