468bfcb10b7522fa516632221aadfdc4b8831afd
azhhng <azhhng@gmail.com>
2023-06-16 21:19:42 -0400

    change styling of page title bar

--- src/components/PageTitleBar/PageTitleBar.jsx
+++ src/components/PageTitleBar/PageTitleBar.jsx
@@ -4,14 +4,22 @@ import Emoji from "../Emoji/Emoji";
 
 function PageTitleBar(props) {
   return (
-    <div
-      className="page-title-container"
-      style={{
-        textDecorationColor: `${props.color}`,
-      }}
-    >
-      <Emoji type={props.emojiType} name={props.emoji} width={50} height={50} />
-      <h1>{props.title}</h1>
+    <div className="page-title-container">
+      <div
+        className="title-bar"
+        style={{
+          textDecorationColor: `${props.color}`,
+        }}
+      >
+        <Emoji
+          type={props.emojiType}
+          name={props.emoji}
+          width={50}
+          height={50}
+        />
+        <h1>{props.title}</h1>
+      </div>
+      <h3>{props.description ?? ""}</h3>
     </div>
   );
 }
