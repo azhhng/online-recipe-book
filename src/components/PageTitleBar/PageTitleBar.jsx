55d0364fc0e94982c3aa23de04be546ffa5b5d62
azhhng <azhhng@gmail.com>
2023-06-25 18:58:56 -0400

    reformat css

--- src/components/PageTitleBar/PageTitleBar.jsx
+++ src/components/PageTitleBar/PageTitleBar.jsx
@@ -5,21 +5,8 @@ import Emoji from "../Emoji/Emoji";
 function PageTitleBar(props) {
   return (
     <div className="page-title-container">
-      <div
-        className="title-bar"
-        style={{
-          textDecorationColor: `${props.color}`,
-        }}
-      >
-        <Emoji
-          type={props.emojiType}
-          name={props.emoji}
-          width={35}
-          height={35}
-        />
-        <h1>{props.title}</h1>
-      </div>
-      <h3>{props.description ?? ""}</h3>
+      <Emoji type={props.emojiType} name={props.emoji} width={30} height={30} />
+      <h1>{props.title}</h1>
     </div>
   );
 }
